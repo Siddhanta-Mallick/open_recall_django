11daa51bd950ba9ffe823136d41acc6ec88d5160
Siddhanta Mallick <sid.mallick1947@gmail.com>
2026-03-27 20:23:40 +0530

    Add setting to allow CSRF

--- open_recall_django/settings.py
+++ open_recall_django/settings.py
@@ -29,6 +29,11 @@ ALLOWED_HOSTS = []
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173', # Good to add both just in case
+]
+
 # Application definition
 
 INSTALLED_APPS = [
