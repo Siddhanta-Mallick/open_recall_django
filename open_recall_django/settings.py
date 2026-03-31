6c347bdfeb19313ae11890046be77f02c847ce8f
Siddhanta Mallick <sid.mallick1947@gmail.com>
2026-03-31 23:47:49 +0530

    Add support for environment variables and gemini api key in settings

--- open_recall_django/settings.py
+++ open_recall_django/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
+GEMINI_API_KEY = env('GEMINI_API_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
